78dbddb9759b73d8418638598f68d197a00c08d8
saipr0 <saiprabhat.05@gmail.com>
2025-05-26 08:20:39 +0530

    add vague.nvim colorscheme

--- init.lua
+++ init.lua
@@ -709,9 +709,10 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'rebelot/kanagawa.nvim',
+    -- 'rebelot/kanagawa.nvim',
     -- 'catppuccin/nvim',
     -- 'folke/tokyonight.nvim',
+    'vague2k/vague.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       -- ---@diagnostic disable-next-line: missing-fields
@@ -724,7 +725,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'kanagawa-dragon'
+      -- vim.cmd.colorscheme 'kanagawa-dragon'
+      vim.cmd.colorscheme 'vague'
       -- vim.cmd.colorscheme 'catppuccin'
       -- vim.cmd.colorscheme 'tokyonight-night'
     end,
